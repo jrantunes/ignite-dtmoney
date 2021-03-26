0fb8022a0142471773c9dbbbc3a8355c535c6654
José Neto <joseantunes2001@gmail.com>
2021-03-26 10:28:44 -0300

    chore: miragejs setup

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from 'react';
 import { Container } from './styles'
 
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then(response => response.json()
+      .then(data => console.log(data)));
+  }, []);
+
   return (
     <Container>
       <table>
